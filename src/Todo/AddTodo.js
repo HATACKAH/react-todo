ac83b6ce8002e822fc08c6b51d6af2e140676ca8
Gleb <godads@yandex.ru>
2020-09-02 14:08:11 +0400

    Create custom input hook

--- src/Todo/AddTodo.js
+++ src/Todo/AddTodo.js
@@ -1,23 +1,35 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
+function useInputValue(defaultValue ='') { //Custom hook
+    const [value, setValue] = useState(defaultValue)
 
+    return {
+        bind: {
+            value,
+            onChange: event=> setValue(event.target.value)
+        },
+        clear: () => setValue(''),
+        value: () => value
+    }
+}
 
 
 function AddTodo ({onCreate}) {
-    const [value, setValue] = useState('')
+    const input = useInputValue('')
 
     function submitHandler(event) {
         event.preventDefault()
 
-        if (value.trim()) {
-            onCreate(value)
-            setValue('')
+        if (input.value().trim ()) {
+            onCreate(input.value())
+            input.clear()
+          
         }
     }
     return (
         <form style={{marginButtom: '1rem'}} onSubmit={submitHandler}>
-            <input value={value} onChange={event=> setValue(event.target.value)} />
+            <input {...input.bind} />
             <button type='submit'>Add task</button>
 
         </form>
